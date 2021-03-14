e7c3269a8da9ae0ee563c49962cc073f97fd619d
longvision <r.n.hori@gmail.com>
2021-03-13 21:34:42 -0300

    https adjust 2

--- server.js
+++ server.js
@@ -24,36 +24,34 @@ const https = require("https");
 const fs = require("fs");
 
 // Certificate
-if (process.env.NODE_ENV === "development") {
-  const pem = require("pem");
-  pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
-    if (error) {
-      return console.log(error);
-    }
 
-    const options = {
-      key: keys.serviceKey,
-      cert: keys.certificate,
-    };
-    new Ignitor(require("@adonisjs/fold"))
-      .appRoot(__dirname)
-      .fireHttpServer((handler) => {
-        return https.createServer(options, handler);
-      })
-      .catch(console.error);
-  });
-}
+const pem = require("pem");
+pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
+  if (error) {
+    return console.log(error);
+  }
 
-if (process.env.NODE_ENV === "production") {
   const options = {
-    key: fs.readFileSync(path.join(__dirname, "./server.key")),
-    cert: fs.readFileSync(path.join(__dirname, "./server.crt")),
+    key: keys.serviceKey,
+    cert: keys.certificate,
   };
-
   new Ignitor(require("@adonisjs/fold"))
     .appRoot(__dirname)
     .fireHttpServer((handler) => {
       return https.createServer(options, handler);
     })
     .catch(console.error);
-}
+});
+
+// PRODUCTION
+//   const options = {
+//     key: fs.readFileSync(path.join(__dirname, "./server.key")),
+//     cert: fs.readFileSync(path.join(__dirname, "./server.crt")),
+//   };
+
+//   new Ignitor(require("@adonisjs/fold"))
+//     .appRoot(__dirname)
+//     .fireHttpServer((handler) => {
+//       return https.createServer(options, handler);
+//     })
+//     .catch(console.error);
